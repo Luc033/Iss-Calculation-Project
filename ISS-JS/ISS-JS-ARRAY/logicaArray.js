d54b652dca3526f96669e248b99271ff72cdf5d2
FalseLuc033 <121646505+FalseLuc033@users.noreply.github.com>
2023-01-02 12:58:33 -0300

    CÁLCULO ISS C/ ARRAY
    ESUDANDO APRIMORAR O SISTEMA POR MEIO DE ARRAY'S COMO FORMA DE SE APROXIMAR DE UM BANCO DE DADOS, PORÉM, VOLÁTIL.

---
+++ ISS-JS/ISS-JS-ARRAY/logicaArray.js
@@ -0,0 +1,13 @@
+var lado = [0];
+var ladoA = 639.78;
+var ladoB = 539.76;
+var inpValorLadoIss = [];
+
+function teste(){
+
+    //ÁREA 
+    var inpArea = parseFloat(document.getElementById('inpArea').value);
+    var entradaNaArray = inpValorLadoIss.push(inpArea);
+    console.log(entradaNaArray, inpValorLadoIss);
+}   
+    
