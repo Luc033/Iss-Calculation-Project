d8e3f14e80c0efdf2d0e9463d7e21f326885b68a
FalseLuc033 <121646505+FalseLuc033@users.noreply.github.com>
2023-01-02 16:39:42 -0300

    Update logicaArray.js

--- ISS-JS/ISS-JS-ARRAY/logicaArray.js
+++ ISS-JS/ISS-JS-ARRAY/logicaArray.js
@@ -1,17 +1,34 @@
-var lado = [0];
-var ladoA = 639.78;
-var ladoB = 539.76;
+// INTERATIVIDADE COM A TABELA
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+}
+
+
+//ÁREA 
+/* COMMENT:
+    A array conseguiu receber o input html mas, somente, com uma var unidimensional auxiliar. Agora, a array segue 
+    manipulável.
+
+*/
 var arInpArea = [];
 
 function teste(){
-
-    //ÁREA 
-    /* COMMENT:
-        A array conseguiu receber o input html mas, somente, com uma var unidimensional auxiliar. Agora, a array segue 
-        manipulável.
-
-    */
     var inpArea = parseFloat(document.getElementById('inpArea').value);
     var auxInpArea = arInpArea.push(inpArea);
-    console.log('auxInpArea: '+auxInpArea+'  |  arInpArea: '+   arInpArea);
+    var total = arInpArea [arInpArea.length-1] + 1;
+
+
+
+    console.log('auxInpArea: '+auxInpArea+'  |  arInpArea: '+   arInpArea [arInpArea.length-1] + '  |  total: '+total.toFixed(2));
+
 }   
+// DECLARANDO FUNÇÃO E VARIAVEIS PARA AUXILIAR O CÁLCULO DE 
+// L-A-D-O
+var arValorLadoIss = [];
+var arLadoChar = [];
+var ladoA = 639.78;
+var ladoB = 539.76;
+
